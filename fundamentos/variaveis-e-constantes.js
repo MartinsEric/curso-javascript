f81d6d26d78204a75e252a621034bf919f878352
unknown <suporte@TJINLA06E11.LABUVA.EDU>
2018-12-12 14:41:23 -0200

    Variaveis e constantes

---
+++ fundamentos/variaveis-e-constantes.js
@@ -0,0 +1,19 @@
+
+
+/* primeira forma de declarar variáveis -> var.
+ * é a forma antiga, portanto, menos usada hoje em dia.
+ */
+var a = 3
+
+/* segunda forma de declarar variáveis -> let.
+ * é a forma mais nova, implementada do ES2015, portanto, a mais usada.
+ */ 
+let b = 4
+
+var a = 5 // o var permite que seja declarada de novo a variável 'a'.
+let b = 6 // o let não permite que seja declarada novamente a variável b.
+
+// declaração de constantes -> const.
+const pi = 3.14
+
+pi = 10 // dará erro! constantes não podem ser alteradas.
