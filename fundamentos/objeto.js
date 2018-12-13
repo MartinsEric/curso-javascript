1ba65390490297bc9dcb74f62483dc4566234f0e
unknown <suporte@TJINLA06E11.LABUVA.EDU>
2018-12-13 11:39:08 -0200

    Objeto

---
+++ fundamentos/objeto.js
@@ -0,0 +1,14 @@
+const produto = {} //criaçao de um objeto vazio
+
+produto.nome = 'televisão' //os atributos podem ser criados dinamicamente, nao precisam obrigatoriamente serem definidos na criaçao do objeto
+produto['preco'] = 2599.99 // outra forma de adicionar um atributo
+
+console.log(produto)
+
+const cidade = {
+    nome: 'Rio de Janeiro',
+    estado: 'Rio de Janeiro',
+    pais: 'Brasil'
+}
+
+console.log(cidade)
