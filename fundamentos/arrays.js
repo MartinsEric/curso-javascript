97a9ccc93e2c8037afcf6739fafb74d68ea4aea0
Eric Martins <ericmartins.martins@gmail.com>
2018-12-12 23:16:30 -0200

    Aula sobre array

---
+++ fundamentos/arrays.js
@@ -0,0 +1,18 @@
+const valores = [5, 7, 9, 3, 10]
+
+console.log(valores[0], valores[3])
+console.log(valores[5]) //retornará undefined pq nao tem posiçao 5.
+
+valores[5] = 6
+console.log(valores[5]) //o JS permite que o array tenha seu tamanho alterado.
+
+valores.push(2.2, 5.4, 'teste', true) //o push insere novos elementos no fim do array.
+console.log(valores)
+
+valores.pop()// remove o ultimo elemento do array e retorna o mesmo.
+console.log(valores)
+
+delete valores[4] // deleta o valor de acordo com o indice passado como parametro.
+console.log(valores)
+
+console.log(valores.length)
