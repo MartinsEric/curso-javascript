ebeabd7a39ab3ed3a2d760bfcd8cb53c805220b7
Eric Martins <ericmartins.martins@gmail.com>
2018-12-18 17:52:53 -0200

    destructuring em arrays

---
+++ fundamentos/destructuring2.js
@@ -0,0 +1,5 @@
+const [a] = [10] // atribuindo o valor de um array unitario na variavel 'a'.
+console.log(a)
+
+const [n1, n2, n3, , n5, n6 = 2] = [10, 23, 14, 15, 12 ]
+console.log(n1, n2, n3, n5, n6)
