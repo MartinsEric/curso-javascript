2288cde39287a7aab2ec7819e8fddf191c95af3c
Eric Martins <ericmartins.martins@gmail.com>
2018-12-18 17:53:24 -0200

    destructuring em arrays

--- fundamentos/destructuring2.js
+++ fundamentos/destructuring2.js
@@ -1,5 +1,5 @@
 const [a] = [10] // atribuindo o valor de um array unitario na variavel 'a'.
 console.log(a)
 
-const [n1, n2, n3, , n5, n6 = 2] = [10, 23, 14, 15, 12 ]
+const [n1, n2, n3, , n5, n6 = 2] = [10, 23, 14, 15, 12 ] // extraindo os valores de um array (o valor 15 e pulado pq uma variavel foi pulada).
 console.log(n1, n2, n3, n5, n6)
