a2fa5e13b4a02ce6e5d292d8f0997510b0912c80
Eric Martins <ericmartins.martins@gmail.com>
2018-12-24 19:36:19 -0200

    parametros padroes

--- funcoes/paramsPadrao.js
+++ funcoes/paramsPadrao.js
@@ -13,8 +13,8 @@ console.log(soma1())
 
 // estrategias 2, 3 e 4 para valores padroes
 function soma2(a, b, c){
-    a = a !== undefined ? a : 1 // 'a' é undefined? senao recebe 1
-    b = 1 in arguments ? b : 1 // 'b' é o indice 1 dos argumentos? senao recebe 1
+    a = a !== undefined ? a : 1 // 'a' é undefined? se for, recebe 1
+    b = 1 in arguments ? b : 1 // 'b' é o indice 1 dos argumentos? se nao, recebe 1
     c = isNaN(c) ? 1 : c // 'c' eh um NaN? entao recebe 1
 
     return a + b + c
