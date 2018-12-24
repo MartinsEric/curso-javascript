8461b52ef15e822a07e0d8c470c82ae8c16297c5
Eric Martins <ericmartins.martins@gmail.com>
2018-12-24 20:18:50 -0200

    usando a funcao bind numa função

---
+++ funcoes/thisEBind2.js
@@ -0,0 +1,12 @@
+function Pessoa(){
+    this.idade = 0
+
+    self = this // fazendo isso, o self sempre vai representar o this da funcao.
+
+    setInterval(function(){
+        self.idade++
+        console.log(self.idade)
+    }/*.bind(this)*/, 1000) // se nao quiser usar o truque do self, so eh necessario usar a funcao bind.
+}
+
+new Pessoa
