a70b83c9ebbf1a65a9c18f5bcbcec2cc90d6707a
Eric Martins <ericmartins.martins@gmail.com>
2018-12-28 13:54:47 -0200

    usando arrow function

---
+++ funcoes/arrowFunction1.js
@@ -0,0 +1,14 @@
+// funcao normal
+let dobro = function(a){
+    return 2 * a
+}
+
+// usando arrow function
+dobro = (a) => {
+    return 2 * a
+}
+console.log(dobro(2))
+
+// reduzindo mais ainda...
+dobro = a => 2 * a
+console.log(dobro(5))
