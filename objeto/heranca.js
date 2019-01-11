ba29b931b6fa582def3957c1dee973412cfca4d0
unknown <suporte@TJENLA02E23.LABUVA.EDU>
2019-01-11 17:04:59 -0200

    heranca - duas formas de definir heranca

---
+++ objeto/heranca.js
@@ -0,0 +1,47 @@
+const avo = { attr1: 'A'}
+const pai = {__proto__: avo, attr2: 'B'}
+const filho = {__proto__: pai, attr3: 'C'}
+
+console.log(filho.attr1)
+console.log(filho.attr2)
+console.log(pai.attr1)
+
+
+const carro = {
+    velAtual: 0,
+    velMax: 200,
+    aceleraMais(delta){
+        if(this.velAtual + delta <= this.velMax){
+            this.velAtual += delta
+        }else{
+            this.velAtual = this.velMax
+        }
+    },
+    status(){
+        return `${this.velAtual}km/h de ${this.velMax}km/h`
+    }
+}
+
+const ferrari = {
+    modelo: 'F40',
+    velMax: 340
+}
+
+const volvo = {
+    modelo: 'V40',
+    status(){
+        return `${this.modelo}: ${super.status()}`
+    }
+}
+
+Object.setPrototypeOf(ferrari, carro)
+Object.setPrototypeOf(volvo, carro)
+
+console.log(ferrari)
+console.log(volvo)
+
+ferrari.aceleraMais(100)
+console.log(ferrari.status())
+
+volvo.aceleraMais(210)
+console.log(volvo.status())
