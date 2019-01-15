8234427b45c49b31d46ab2ff1042336bea98d33b
unknown <suporte@TJENLA02E23.LABUVA.EDU>
2019-01-15 12:29:09 -0200

    array

---
+++ array/array.js
@@ -0,0 +1,8 @@
+ const aprovados = ['Eric', 'Luiza', 'Breno', 'Fabrício']
+
+ console.log(aprovados)
+ aprovados.sort() //Ordenada os elementos.
+ console.log(aprovados)
+
+ aprovados.splice(1, 2, 'elemento1', 'elemento2')
+ console.log(aprovados)
